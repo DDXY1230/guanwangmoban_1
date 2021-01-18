46c4aedd6ec25671a945b01abbefdce0ed6222ec
DDXY1230 <2502752288@qq.com>
2021-01-18 10:25:49 +0800

    配置svg图标

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import i18n from './languages/index.js'
+import '@/icons'
 
 Vue.use(ElementUI);
 
