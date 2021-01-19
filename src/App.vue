788174ba563a69f3fe6131b16873f6a5df7ae0a9
DDXY1230 <2502752288@qq.com>
2021-01-19 10:56:37 +0800

    修改底部

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
     <Header></Header>
     <div class="main">
       <router-view/>
+      <svg-icon iconClass="close"></svg-icon>
+
     </div>
     <Footer></Footer>
   </div>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,31 @@
 <template>
-  <div class="footer">{{$t('footer.text')}}</div>
+  <div class="footer">
+    <img src="@/assets/imgs/logo.png" alt="">
+    <p>© 2020-2020 易临云（深圳）科技有限公司 版权所有 粤ICP备20068916号.</p>
+    <p>
+      <svg-icon iconClass="phone"></svg-icon>
+      028-11123233
+      {{someSpace}}
+      <svg-icon iconClass="email"></svg-icon>
+      341341241@123.com
+    </p>
+  </div>
 </template>
 <script>
-  export default {
-    name: 'Footer'
+export default {
+  name: "Footer",
+  data() {
+    return {
+      someSpace: `\xa0\xa0\xa0`
+    };
   }
+};
 </script>
 <style lang="scss" scoped>
 .footer {
-  height: 60px;
-  line-height: 60px;
-  background: #2C354D;
+  background: #454545;
   color: #fff;
   text-align: center;
+  padding: 50px 0;
 }
 </style>
